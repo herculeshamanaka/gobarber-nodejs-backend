d33837d917750f746d07812aba2d9c5afed8e358
Hercules Neves Hamanaka <hamanhrc@schaeffler.com>
2020-05-21 22:39:08 -0300

    feature: applying Liskov principle

---
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -0,0 +1,22 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = await this.ormRepository.findOne({
+      where: { date },
+    });
+    return findAppointment;
+  }
+}
+
+export default AppointmentsRepository;
